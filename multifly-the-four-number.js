9e5d90aa44733115654b6e523ad80ed3802425a1
helal9380 <asadurrahman9380@gmail.com>
2024-02-02 22:45:34 +0600

    added the multifly of the four numbers using the function

---
+++ multifly-the-four-number.js
@@ -0,0 +1,11 @@
+// Task-1
+// Take four parameters. Multiply the four numbers and then return the result
+
+function multiply(number1, number2, number3, number4) {
+    const multi = number1 * number2 * number3 * number4;
+    return multi
+}
+const multiplyResult = multiply(1, 2, 4, 6);
+
+// output : 48;
+console.log(multiplyResult)
